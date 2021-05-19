50757f95c18f508ff26da1dcf9924601301b13e8
kzlp-ldz <kamilyazakirova15935700@gmail.com>
2021-05-19 13:06:58 +0300

    добавила классы

--- SmtNew/Form1.Designer.cs
+++ SmtNew/Form1.Designer.cs
@@ -30,26 +30,145 @@ namespace SmtNew
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.pnl_Draw = new System.Windows.Forms.Panel();
+            this.btn_load = new System.Windows.Forms.Button();
+            this.btn_safe = new System.Windows.Forms.Button();
+            this.num_width = new System.Windows.Forms.NumericUpDown();
+            this.btn_color = new System.Windows.Forms.Button();
+            this.lb_choose = new System.Windows.Forms.ListBox();
+            this.figureData = new System.Windows.Forms.DataGridView();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.key = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.val = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pnl_Draw.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.num_width)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.figureData)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.LavenderBlush;
-            this.panel1.Location = new System.Drawing.Point(-17, -12);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1489, 813);
+            this.panel1.Size = new System.Drawing.Size(1113, 561);
             this.panel1.TabIndex = 0;
             this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
+            // 
+            // pnl_Draw
+            // 
+            this.pnl_Draw.Controls.Add(this.btn_load);
+            this.pnl_Draw.Controls.Add(this.btn_safe);
+            this.pnl_Draw.Controls.Add(this.num_width);
+            this.pnl_Draw.Controls.Add(this.btn_color);
+            this.pnl_Draw.Controls.Add(this.lb_choose);
+            this.pnl_Draw.Controls.Add(this.figureData);
+            this.pnl_Draw.Location = new System.Drawing.Point(750, -5);
+            this.pnl_Draw.Name = "pnl_Draw";
+            this.pnl_Draw.Size = new System.Drawing.Size(363, 568);
+            this.pnl_Draw.TabIndex = 1;
+            // 
+            // btn_load
+            // 
+            this.btn_load.Location = new System.Drawing.Point(196, 457);
+            this.btn_load.Name = "btn_load";
+            this.btn_load.Size = new System.Drawing.Size(117, 49);
+            this.btn_load.TabIndex = 5;
+            this.btn_load.Text = "Load";
+            this.btn_load.UseVisualStyleBackColor = true;
+            this.btn_load.Click += new System.EventHandler(this.btn_load_Click);
+            // 
+            // btn_safe
+            // 
+            this.btn_safe.Location = new System.Drawing.Point(37, 457);
+            this.btn_safe.Name = "btn_safe";
+            this.btn_safe.Size = new System.Drawing.Size(112, 48);
+            this.btn_safe.TabIndex = 4;
+            this.btn_safe.Text = "Safe";
+            this.btn_safe.UseVisualStyleBackColor = true;
+            // 
+            // num_width
+            // 
+            this.num_width.Location = new System.Drawing.Point(8, 397);
+            this.num_width.Name = "num_width";
+            this.num_width.Size = new System.Drawing.Size(343, 22);
+            this.num_width.TabIndex = 3;
+            this.num_width.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // btn_color
+            // 
+            this.btn_color.BackColor = System.Drawing.Color.Black;
+            this.btn_color.Location = new System.Drawing.Point(8, 342);
+            this.btn_color.Name = "btn_color";
+            this.btn_color.Size = new System.Drawing.Size(343, 23);
+            this.btn_color.TabIndex = 2;
+            this.btn_color.UseVisualStyleBackColor = false;
+            // 
+            // lb_choose
+            // 
+            this.lb_choose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lb_choose.FormattingEnabled = true;
+            this.lb_choose.ItemHeight = 22;
+            this.lb_choose.Items.AddRange(new object[] {
+            "Circle",
+            "Line",
+            "Treangle",
+            "Rectangle"});
+            this.lb_choose.Location = new System.Drawing.Point(8, 17);
+            this.lb_choose.Name = "lb_choose";
+            this.lb_choose.Size = new System.Drawing.Size(343, 92);
+            this.lb_choose.TabIndex = 1;
+            this.lb_choose.SelectedIndexChanged += new System.EventHandler(this.lb_choose_SelectedIndexChanged);
+            // 
+            // figureData
+            // 
+            this.figureData.AllowUserToDeleteRows = false;
+            this.figureData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.figureData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.key,
+            this.val});
+            this.figureData.Location = new System.Drawing.Point(8, 126);
+            this.figureData.Name = "figureData";
+            this.figureData.ReadOnly = true;
+            this.figureData.RowHeadersWidth = 51;
+            this.figureData.RowTemplate.Height = 24;
+            this.figureData.Size = new System.Drawing.Size(343, 192);
+            this.figureData.TabIndex = 0;
+            this.figureData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.figureData_CellContentClick);
+            // 
+            // key
+            // 
+            this.key.HeaderText = "";
+            this.key.MinimumWidth = 6;
+            this.key.Name = "key";
+            this.key.ReadOnly = true;
+            this.key.Width = 125;
+            // 
+            // val
+            // 
+            this.val.HeaderText = "";
+            this.val.MinimumWidth = 6;
+            this.val.Name = "val";
+            this.val.ReadOnly = true;
+            this.val.Width = 125;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1113, 561);
+            this.Controls.Add(this.pnl_Draw);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnl_Draw.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.num_width)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.figureData)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +176,16 @@ namespace SmtNew
         #endregion
 
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnl_Draw;
+        private System.Windows.Forms.DataGridView figureData;
+        private System.Windows.Forms.Button btn_color;
+        private System.Windows.Forms.ListBox lb_choose;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.NumericUpDown num_width;
+        private System.Windows.Forms.Button btn_load;
+        private System.Windows.Forms.Button btn_safe;
+        private System.Windows.Forms.DataGridViewTextBoxColumn key;
+        private System.Windows.Forms.DataGridViewTextBoxColumn val;
     }
 }
 
--- SmtNew/Form1.cs
+++ SmtNew/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SmtNew
 {
+    
     public partial class Form1 : Form
     {
-        int radisu = 50;
         public Form1()
         {
             InitializeComponent();
@@ -26,9 +26,218 @@ namespace SmtNew
 
         private void panel1_MouseClick(object sender, MouseEventArgs e)
         {
-            Graphics graphics = panel1.CreateGraphics();
-            graphics.FillEllipse(Brushes.Black, e.X, e.Y, radisu * 2, radisu * 2);
-            graphics.Dispose();
+            
+        }
+
+        private void figureData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lb_choose_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Figure
+    {
+        protected int x;
+        protected int y;
+
+        public Figure(int newX, int newY)
+        {
+            x = newX;
+            y = newY;
+        }
+
+        public virtual void Draw()
+        { }
+
+        public override string ToString()
+        {
+            return base.ToString() + $";X={x};Y={y};";
+        }
+    }
+
+    public class Line : Figure
+    {
+        protected int x2;
+        protected int y2;
+
+        public Line(int newX, int newY, int newX2, int newY2) : base(newX, newY)
+        {
+            x2 = newX2;
+            y2 = newY2;
+        }
+
+        public override void Draw()
+        {
+            // Draw line
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"X2={x2};Y2={y2};";
+        }
+    }
+    public class Rectangle : Figure
+    {
+        protected int x2;
+        protected int y2;
+
+        public Rectangle(int newX, int newY, int newX2, int newY2) : base(newX, newY)
+        {
+            x2 = newX2;
+            y2 = newY2;
+        }
+
+        public override void Draw()
+        {
+            // Draw rectangle
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"X2={x2};Y2={y2};";
+        }
+    }
+    public class Treangle : Figure
+    {
+        protected int x2;
+        protected int y2;
+
+        public Treangle(int newX, int newY, int newX2, int newY2) : base(newX, newY)
+        {
+            x2 = newX2;
+            y2 = newY2;
+        }
+
+        public override void Draw()
+        {
+            // Draw treangle
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"X2={x2};Y2={y2};";
+        }
+    }
+
+    public class Circle : Figure
+    {
+        protected int r;
+
+        public Circle(int newX, int newY, int newR) : base(newX, newY)
+        {
+            r = newR;
+        }
+
+        public override void Draw()
+        {
+            // Draw Circle
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"R={r}";
+        }
+    }
+    public class FiguresData
+    {
+        public string Name;
+        public Dictionary<string, int> Data;
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    static public class FigureFab
+    {
+        public static Figure Make(FiguresData figData)
+        {
+            Figure fig = null;
+
+            switch (figData.Name)
+            {
+                case "Line":
+                    fig = new Line(figData.Data["X1"], figData.Data["Y2"], figData.Data["X2"], figData.Data["Y2"]);
+                    break;
+                case "Circle":
+                    fig = new Circle(figData.Data["X"], figData.Data["Y"], figData.Data["Radius"]);
+                    break;
+                case "Rectangle":
+                    fig = new Rectangle(figData.Data["X1"], figData.Data["Y1"], figData.Data["X2"], figData.Data["Y2"]);
+                    break;
+                case "Treangle":
+                    fig = new Treangle(figData.Data["X1"], figData.Data["Y1"], figData.Data["X2"], figData.Data["Y2"]);
+                    break;
+            }
+
+            return fig;
+        }
+
+        public static List<FiguresData> InitFiguresData()
+        {
+            var figuresData = new List<FiguresData>();
+
+            figuresData.Add(new FiguresData
+            {
+                Name = "Circle",
+                Data = new Dictionary<string, int>
+                {
+                    { "X", 0 },
+                    { "Y", 0 },
+                    { "Radius", 0 }
+                }
+            });
+
+            figuresData.Add(new FiguresData
+            {
+                Name = "Line",
+                Data = new Dictionary<string, int>
+                {
+                    { "X1", 0 },
+                    { "Y1", 0 },
+                    { "X2", 0 },
+                    { "Y2", 0 },
+                }
+            });
+
+            figuresData.Add(new FiguresData
+            {
+                Name = "Rectangle",
+                Data = new Dictionary<string, int>
+                {
+                    { "X1", 0 },
+                    { "Y1", 0 },
+                    { "X2", 0 },
+                    { "Y2", 0 },
+                }
+            });
+
+            figuresData.Add(new FiguresData
+            {
+                Name = "Treangle",
+                Data = new Dictionary<string, int>
+                {
+                    { "X", 0 },
+                    { "Y", 0 },
+                }
+            });
+
+            return figuresData;
         }
     }
 }
